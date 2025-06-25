836fd0c382923b415bbcb44b9a4c8c07000b0c4c
Geoff Little <g.little712@gmail.com>
2025-06-25 15:42:21 +0000

    fix: mkdir otel-output in run.sh

--- run-otel-logger.sh
+++ run-otel-logger.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+mkdir -p "$(pwd)/otel-output"
+chmod 777 "$(pwd)/otel-output"
+
 docker run --rm \
   -v "$(pwd)/file-collector.yml":/etc/otel-collector-config.yml \
   -v "$(pwd)/otel-output":/etc/otel-collector \
