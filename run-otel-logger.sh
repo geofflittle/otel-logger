71551df053741968c2f3d8471106973ae4dee535
Geoff Little <g.little712@gmail.com>
2025-07-18 23:11:54 +0000

    Fix run script perms

--- run-otel-logger.sh
+++ run-otel-logger.sh
@@ -2,9 +2,9 @@
 set -e
 
 mkdir -p "$(pwd)/otel-output"
-chmod 777 "$(pwd)/otel-output"
 
 docker run --rm \
+  -u $(id -u):$(id -g) \
   -v "$(pwd)/file-collector.yml":/etc/otel-collector-config.yml \
   -v "$(pwd)/otel-output":/etc/otel-collector \
   -p 4317:4317 \
